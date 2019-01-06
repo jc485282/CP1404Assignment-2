3ab85fb7f56d7884e35322531631e3fc40aa7802
jc485282 <richa.sharma1@my.jcu.edu.au>
2019-01-06 19:49:36 +0530

    added Method complete song count add method sorting

--- Songslist.py
+++ Songslist.py
@@ -17,10 +17,34 @@ class SongList:
                 return song[0]
 
     def tolearn_songs_count(self):
-        # Loops  songs list and counts all the songs that needs to be learned and return count
+
         song_tolearn = 0
         for song in self.songs:
             if song[0].status == 'y':
                 song_tolearn += 1
         return song_tolearn
+
+    def learned_songs_count(self):
+
+        learned_songs = 0
+        for song in self.songs:
+            if song[0].status == 'n':
+                learned_songs += 1
+        return learned_songs
+
+
+    def load_songs(self):
+
+        filereader = open('song.csv', 'r')
+        for song in filereader:
+            song_string = song.split(",")
+            self.songs.append(
+                [Song(song_string[0], song_string[1], int(song_string[2]), song_string[3].strip())])
+
+        filereader.close()
+
+
+
+
+
 
